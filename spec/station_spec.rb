5016683287082e4ef5f2006c5382bdabd8017530
Noa Farber <noaros88@gmail.com>
2021-04-07 17:43:56 +0100

    created a station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,12 @@
+require 'station'
+describe Station do
+let(:subject) { Station.new(3, "Victoria") }
+  it 'recognise the zone' do
+    expect(subject.zone).to eq(3)
+  end
+
+  it 'recognise the name' do
+    expect(subject.name).to eq("Victoria")
+  end
+
+end
